284ec2877de40c9bd1710623ac177cd314819238
danielmejia1985 <daniel.mejiapalacios1985@gmail.com>
2023-11-27 06:49:12 -0600

    Modificacion en homework React intro exercises

--- FT-M2-master/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
+++ FT-M2-master/06-React-Intro/homework/01 - Exercises/src/components/Bienvenido.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
-const studentName = "";
+const studentName = "DANIEL MEJIA";
 const techSkills = ["Html", "Css", "JavaScript", "React", "Redux"];
 const alerts = { m1: "Aprobado", m2: "En curso" };
 
 export default function Bienvenido() {
   // el código de tu componente acá
-  return;
+  return (
+    <div>
+      <h1>Iniciando React</h1>
+    </div>
+  );
 }
 
 // Esto lo exportamos para los tests
