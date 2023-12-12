b2122a4442e89843f658ef10169e97fbff732e2f
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-12-12 12:11:53 -0500

    add hw node done

--- FT-M3-master/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/bash.js
@@ -1,11 +1,36 @@
-const process = require('process');
-const { Z_ASCII } = require('zlib');
-const commands = require('./commands/index.js');
+const process = require("process");
+const { Z_ASCII } = require("zlib");
+const commands = require("./commands/index.js");
+// commands -> {pwd: fn(), date:fn(), echo, ls, cat, head, tail, curl}
+// > echo hola
+// hola
+// >
+function bash() {
+  process.stdout.write("prompt > ");
+  process.stdin.on("data", function (data) {
+    // data -> buffer
+    const args = data.toString().trim();
+    // args -> "echo hola mundo"
+    // args.split -> ["echo", "hola", "mundo"]
+    const [cmd, ...Arrtext] = args.split(" ");
+    // text -> ["hola", "mundo"]
+    const text = Arrtext.join(" ");
+    // text -> "hola mundo"
+    // Verifiquemos que cmd si es un comando
+    if (commands[cmd]) {
+      commands[cmd](print, text);
+    } else {
+      print(`command not found: ${cmd}`);
+    }
+  });
+}
 
-function bash() {}
-
+function print(output) {
+  process.stdout.write(output);
+  process.stdout.write("\nprompt > ");
+}
 bash();
 module.exports = {
-   print,
-   bash,
+  print,
+  bash,
 };
--- FT-M3-master/01-Node/homework/01 - Exercises/commands/index.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,65 @@
 const fs = require("fs");
 const utils = require("../utils/request");
+// utils -> {request : fn(url, cb)}
 const process = require("process");
 
-function pwd() {}
+function pwd(print) {
+  print(process.cwd());
+}
 
-function date() {}
+function date(print) {
+  print(Date());
+}
 
-function echo() {}
+function echo(print, data) {
+  print(data);
+}
 
-function ls() {}
+function reusableFT45b(type, print, err, data) {
+  if (err) throw new Error(err);
+  else {
+    switch (type) {
+      case "ls":
+        return print(data.join(" "));
+      case "cat":
+        return print(data);
+      case "head":
+        return print(data.split("\n")[0]);
+      case "tail":
+        return print(data.split("\n").at(-1).trim());
+    }
+  }
+}
 
-function cat() {}
+function ls(print) {
+  fs.readdir(".", (err, data) => reusableFT45b("ls", print, err, data));
+}
 
-function head() {}
+function cat(print, path) {
+  fs.readFile(path, "utf-8", (err, data) =>
+    reusableFT45b("cat", print, err, data)
+  );
+}
 
-function tail() {}
+function head(print, path) {
+  fs.readFile(path, "utf-8", (err, data) =>
+    reusableFT45b("head", print, err, data)
+  );
+}
 
-function curl() {}
+function tail(print, path) {
+  fs.readFile(path, "utf-8", (err, data) =>
+    reusableFT45b("tail", print, err, data)
+  );
+}
 
-module.exports = {};
+function curl(print, path) {
+  utils.request(path, (err, data) => {
+    if (err) throw new Error(err);
+    else {
+      print(data);
+    }
+  });
+}
+
+module.exports = { pwd, date, echo, ls, cat, head, tail, curl };
