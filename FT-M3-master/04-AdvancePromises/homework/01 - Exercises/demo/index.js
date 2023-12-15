8c7f65df40297f1e73032c9e9fb69b778e765224
Felipe Ciro Montoya <fmontoya@soyhenry.com>
2023-12-15 11:42:18 -0500

    hw advance promises done

---
+++ FT-M3-master/04-AdvancePromises/homework/01 - Exercises/demo/index.js
@@ -0,0 +1,15 @@
+const $Promise = require("../src/pledge");
+
+console.log($Promise);
+const boolean = false;
+const myPromise = new $Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (boolean) resolve("bien");
+    else reject("mal");
+  }, 1000);
+});
+
+console.log(myPromise);
+
+myPromise.then().then().then(() => { })
+    // [{successCb:()=> {}, errorCb: false}]->
--- FT-M3-master/04-AdvancePromises/homework/01 - Exercises/src/pledge.js
+++ FT-M3-master/04-AdvancePromises/homework/01 - Exercises/src/pledge.js
@@ -1,10 +1,64 @@
-'use strict';
+"use strict";
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // // TU CÓDIGO AQUÍ:
 
+function $Promise(executor) {
+  if (typeof executor !== "function") {
+    throw new TypeError("executor must be function");
+  }
+  this._state = "pending";
+  this._handlerGroups = [];
+  executor(this._internalResolve.bind(this), this._internalReject.bind(this));
+}
 
+$Promise.prototype._internalResolve = function (value) {
+  if (this._state === "pending") {
+    this._state = "fulfilled";
+    this._value = value;
+    this._callHandlers();
+  }
+};
+
+$Promise.prototype._internalReject = function (reason) {
+  if (this._state === "pending") {
+    this._state = "rejected";
+    this._value = reason;
+    this._callHandlers();
+  }
+};
+
+$Promise.prototype.then = function (successCb, errorCb) {
+  if (typeof successCb !== "function") successCb = false;
+  if (typeof errorCb !== "function") errorCb = false;
+  this._handlerGroups.push({
+    successCb,
+    errorCb,
+  });
+  //? En que momento es que se debe disparar un then?
+  if (this._state !== "pending") this._callHandlers();
+};
+
+$Promise.prototype._callHandlers = function () {
+  while (this._handlerGroups.length) {
+    const handler = this._handlerGroups.shift();
+    // handler -> {successCb: false , errorCb: }
+    if (this._state === "fulfilled") {
+      if (handler.successCb) {
+        handler.successCb(this._value);
+      }
+    } else if (this._state === "rejected") {
+      if (handler.errorCb) {
+        handler.errorCb(this._value);
+      }
+    }
+  }
+};
+
+$Promise.prototype.catch = function (errorCb) {
+  this.then(null, errorCb);
+};
 
 module.exports = $Promise;
 /*-------------------------------------------------------
